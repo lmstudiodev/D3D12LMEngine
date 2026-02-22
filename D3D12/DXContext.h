3cc2d436df1dba07f91832844a056e0e7893a631
Gino Preti <lmstudiodev@gmail.com>
2026-02-22 10:43:57 +0100

    Added DXContext class

--- D3D12/DXContext.h
+++ D3D12/DXContext.h
@@ -25,6 +25,13 @@ public:
 	bool Init();
 	void ShutDown();
 
+	inline ComPointer<ID3D12Device10>& GetDevice() { return m_device; }
+	inline ComPointer<ID3D12CommandQueue>& GetCommandQueue() { return m_cmdQueue; }
+
 private:
 	ComPointer<ID3D12Device10> m_device;
+	ComPointer<ID3D12CommandQueue> m_cmdQueue;
+	ComPointer<ID3D12Fence1> m_fence;
+
+	UINT64 m_fenceValue = 0;
 };
