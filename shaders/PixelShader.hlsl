cfd7609f661507843f89ff65a63b826cbfbc5c42
lmstudiodev <lmstudiodev@gmail.com>
2026-02-25 14:56:46 +0100

    Set Viewport, shader update, vertex structure update

--- shaders/PixelShader.hlsl
+++ shaders/PixelShader.hlsl
@@ -1,7 +1,14 @@
 #include "RootSignature.hlsl"
 
-[RootSignature(ROOTSIG)]
-float4 main() : SV_TARGET
+struct VertexOut
 {
-	return float4(1.0f, 1.0f, 1.0f, 1.0f);
+    float4 pos : SV_Position;
+    float3 color : COLOR;
+    float2 textCoord : TEXCOORD;
+};
+
+[RootSignature(ROOTSIG)]
+float4 main(VertexOut Input) : SV_TARGET
+{
+    return float4(Input.color, 1.0f);
 }
